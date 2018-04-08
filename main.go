0c637f461b49ff32d74b03c722fc6832a3e2a666
Krzysztof Grodzicki <kgrodzicki@gmail.com>
2018-04-08 21:21:17 +0200

    mock AWS requests, simple tests

--- main.go
+++ main.go
@@ -4,12 +4,51 @@ import (
 	"log"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
+	"fmt"
+	"github.com/aws/aws-sdk-go/service/cloudwatchlogs/cloudwatchlogsiface"
 )
 
 func Handler(request events.CloudWatchEvent) (string, error) {
 	log.Printf("Processing Lambda request %s\n", request)
-	// TODO [grokrz]: implement
+
+	sess := session.Must(session.NewSession())
+
+	client := cloudwatchlogs.New(sess)
+
+	logGroups, err := GetLogGroups(client)
+	check(err)
+
+	subscriptionFilters, err := DescribeSubscriptionFilters(client)
+	check(err)
+
+	fmt.Print(logGroups, subscriptionFilters)
+
+	// TODO [grokrz]: impl
+
 	return "Event processed", nil
+}
+
+func GetLogGroups(logs cloudwatchlogsiface.CloudWatchLogsAPI) (*cloudwatchlogs.DescribeLogGroupsOutput, error) {
+	input := cloudwatchlogs.DescribeLogGroupsInput{}
+	return logs.DescribeLogGroups(&input)
+}
+
+func DescribeSubscriptionFilters(logs cloudwatchlogsiface.CloudWatchLogsAPI) (*cloudwatchlogs.DescribeSubscriptionFiltersOutput, error) {
+	input := cloudwatchlogs.DescribeSubscriptionFiltersInput{}
+	return logs.DescribeSubscriptionFilters(&input)
+}
+
+func PutSubscriptionFilter(logs cloudwatchlogsiface.CloudWatchLogsAPI) (*cloudwatchlogs.PutSubscriptionFilterOutput, error) {
+	input := cloudwatchlogs.PutSubscriptionFilterInput{}
+	return logs.PutSubscriptionFilter(&input)
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
--- main_test.go
+++ main_test.go
@@ -4,41 +4,64 @@ import (
 	"testing"
 	"io/ioutil"
 	"log-group-subscriber"
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/stretchr/testify/assert"
 	"encoding/json"
+	"github.com/aws/aws-sdk-go/service/cloudwatchlogs/cloudwatchlogsiface"
+	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestHandler(t *testing.T) {
-	var scheduledEvent events.CloudWatchEvent
-	var inputJson = readJsonFromFile(t, "testdata/scheduled-event.json")
+//func TestHandler(t *testing.T) {
+//	var scheduledEvent events.CloudWatchEvent
+//	var inputJson = readJsonFromFile(t, "testdata/scheduled-event.json")
+//
+//	var err = json.Unmarshal(inputJson, &scheduledEvent)
+//	check(t, err)
+//
+//	tests := []struct {
+//		request events.CloudWatchEvent
+//		expect  string
+//		err     error
+//	}{
+//		{
+//			request: scheduledEvent,
+//			expect:  "Event processed",
+//			err:     nil,
+//		},
+//	}
+//
+//	for _, test := range tests {
+//		response, err := main.Handler(test.request)
+//		assert.IsType(t, test.err, err)
+//		assert.Equal(t, test.expect, response)
+//	}
+//}
 
-	var err = json.Unmarshal(inputJson, &scheduledEvent)
+func TestGetLogGroups(t *testing.T) {
+	cwl := &mockCloudWatchLogsClient{}
+
+	logGroups, err := main.GetLogGroups(cwl)
+
 	check(t, err)
-
-	tests := []struct {
-		request events.CloudWatchEvent
-		expect  string
-		err     error
-	}{
-		{
-			request: scheduledEvent,
-			expect:  "Event processed",
-			err:     nil,
-		},
-	}
-
-	for _, test := range tests {
-		response, err := main.Handler(test.request)
-		assert.IsType(t, test.err, err)
-		assert.Equal(t, test.expect, response)
-	}
+	assert.NotNil(t, logGroups)
+	assert.Equal(t, 2, len(logGroups.LogGroups))
 }
 
-func readJsonFromFile(t *testing.T, inputFile string) ([]byte) {
-	inputJson, err := ioutil.ReadFile(inputFile)
+func TestDescribeSubscriptionFilters(t *testing.T) {
+	cwl := &mockCloudWatchLogsClient{}
+
+	subscriptionFilters, err := main.DescribeSubscriptionFilters(cwl)
+
 	check(t, err)
-	return inputJson
+	assert.Equal(t, 1, len(subscriptionFilters.SubscriptionFilters))
+}
+
+func TestPutSubscriptionFilter(t *testing.T) {
+	cwl := &mockCloudWatchLogsClient{}
+
+	res, err := main.PutSubscriptionFilter(cwl)
+
+	check(t, err)
+	assert.NotNil(t, res)
 }
 
 func check(t *testing.T, err error) {
@@ -46,4 +69,41 @@ func check(t *testing.T, err error) {
 		t.Errorf("could not open test file. details: %v", err)
 		panic(err)
 	}
+}
+
+// Define a mock struct to be used in your unit tests of myFunc.
+type mockCloudWatchLogsClient struct {
+	cloudwatchlogsiface.CloudWatchLogsAPI
+}
+
+func (m *mockCloudWatchLogsClient) DescribeLogGroups(input *cloudwatchlogs.DescribeLogGroupsInput) (*cloudwatchlogs.DescribeLogGroupsOutput, error) {
+	var inputJson = readFile("testdata/describeLogGroups-output.json")
+	var describeLogGroupsOutput cloudwatchlogs.DescribeLogGroupsOutput
+	err := json.Unmarshal(inputJson, &describeLogGroupsOutput)
+	if err != nil {
+		panic(err)
+	}
+	return &describeLogGroupsOutput, nil
+}
+
+func (m *mockCloudWatchLogsClient) DescribeSubscriptionFilters(*cloudwatchlogs.DescribeSubscriptionFiltersInput) (*cloudwatchlogs.DescribeSubscriptionFiltersOutput, error) {
+	var inputJson = readFile("testdata/describeSubscriptionFilters-output.json")
+	var describeSubscriptionFiltersOutput cloudwatchlogs.DescribeSubscriptionFiltersOutput
+	err := json.Unmarshal(inputJson, &describeSubscriptionFiltersOutput)
+	if err != nil {
+		panic(err)
+	}
+	return &describeSubscriptionFiltersOutput, nil
+}
+
+func (m *mockCloudWatchLogsClient) PutSubscriptionFilter(input *cloudwatchlogs.PutSubscriptionFilterInput) (*cloudwatchlogs.PutSubscriptionFilterOutput, error) {
+	return &cloudwatchlogs.PutSubscriptionFilterOutput{}, nil
+}
+
+func readFile(path string) ([]byte) {
+	f, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	return f
 }
